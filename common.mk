08a93d359127430efab0a5d3aa3ee372d710529d
Deivid21 <david.parra.ignacio@gmail.com>
2021-01-03 03:08:12 +0530

    sm8350-common: Add TurboPowerOverlays from stock
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Naxo79 <nacho79.garcia@gmail.com>
    Change-Id: Ia262182e86757e16ac6e06d37aa59c0683f98943
    Signed-off-by: Naxo79 <nacho79.garcia@gmail.com>

--- common.mk
+++ common.mk
@@ -238,7 +238,8 @@ PRODUCT_COPY_FILES += \
 # Moto hardware
 PRODUCT_PACKAGES += \
     MotoActions \
-    MotoCommonOverlay
+    MotoCommonOverlay \
+    TurboPowerOverlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
