95edfe05b67a36482421e119c6e3d123c221414d
Deivid21 <david.parra.ignacio@gmail.com>
2025-06-26 02:05:04 -0400

    sm7325-common: Copy init media files with blueprint
    Change-Id: I16396e98aabd108a334b530e22d787ce689d8369

--- common.mk
+++ common.mk
@@ -223,9 +223,9 @@ $(call soong_config_set,livedisplay_sdm,enable_dm,false)
 $(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
 # Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh \
-    $(LOCAL_PATH)/media/init.qti.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.media.rc
+PRODUCT_PACKAGES += \
+    init.qti.media.rc \
+    init.qti.media.sh
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/lahaina/media_codecs_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
